f569e6388fe29c4716fcecbee70c026937d5810c
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-06 16:55:38 -0700

    Add more info to weather

--- app/models/weather.js
+++ app/models/weather.js
@@ -3,6 +3,7 @@ export default class Weather {
     this.city = data.name
     this.tempInF = Math.round((data.main.temp - 273.15) * 9 / 5 + 32)
     this.icon = data.weather[0].icon
+    this.description = data.weather[0].description
   }
 
   get Template() {
@@ -10,6 +11,7 @@ export default class Weather {
       <image src="http://openweathermap.org/img/wn/${this.icon}.png"></image>
       <h1 class="d-inline-block mb-0">${this.tempInF}°F</h1>
       <h3>${this.city}</h3>
+      <p>${this.description.charAt(0).toUpperCase() + this.description.substring(1)}</p>
     `
   }
 
