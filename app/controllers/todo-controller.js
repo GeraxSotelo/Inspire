b5b6beec4106b1fe99c10b5b31e8c4dd8779bfba
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-08 18:37:31 -0700

    Setup count todos function

--- app/controllers/todo-controller.js
+++ app/controllers/todo-controller.js
@@ -1,10 +1,34 @@
 import TodoService from "../services/todo-service.js";
 import store from "../store.js";
 
+function _todoCount(total, completed) {
+  let template = ''
+  let todoText = 'To Do'
+  if (total > 1) {
+    todoText += 's'
+  }
+  if (total > 0) {
+    template += `${total} ${todoText}. `
+    if (completed > 0) {
+      template += `${completed} completed.`
+    }
+  }
+  document.querySelector("#todo-count").innerText = template
+}
+
 function _drawTodos() {
   let template = ''
+  let totalCount = 0
+  let completedCount = 0
   let todo = store.State.todos
-  todo.forEach(t => template += t.Template)
+  todo.forEach(t => {
+    template += t.Template
+    totalCount++
+    if (t.completed == true) {
+      completedCount++
+    }
+  })
+  _todoCount(totalCount, completedCount)
   document.querySelector("#todos").innerHTML = template
 }
 
