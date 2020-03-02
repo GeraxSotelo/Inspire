f9f3aceb296e209c1a2c3f3e9b5c06cbda20028b
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-02 12:52:38 -0700

    Replace weather api

--- app/services/quote-service.js
+++ app/services/quote-service.js
@@ -3,7 +3,6 @@ import Quote from "../models/quote.js";
 
 // @ts-ignore
 const _quoteApi = axios.create({
-  // baseURL: "//bcw-sandbox.herokuapp.com/api/quotes",
   baseURL: "https://type.fit/api/quotes",
   timeout: 3000
 });
