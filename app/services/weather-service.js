89f63785761b6dce4f1b66662339afccef7d43c0
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-02 16:03:52 -0700

    fixed weather api link

--- app/services/weather-service.js
+++ app/services/weather-service.js
@@ -3,7 +3,7 @@ import store from "../store.js";
 
 // @ts-ignore
 const weatherApi = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/weather?lat=43.6135002&lon=-116.2034505&appid=fcb38c99a81c13ba38e4e647423ae0a1",
+  baseURL: "https://api.openweathermap.org/data/2.5/weather?lat=43.6135002&lon=-116.2034505&appid=fcb38c99a81c13ba38e4e647423ae0a1",
   timeout: 3000
 });
 
